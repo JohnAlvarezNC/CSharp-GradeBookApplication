7124c014d5d592e5b6a749a71ae84808310b3d4f
JohnAlvarezNC <62408298+JohnAlvarezNC@users.noreply.github.com>
2020-03-20 10:35:10 +0800

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,25 @@ namespace GradeBook.GradeBooks
         {
             base.Type = Enums.GradeBookType.Ranked;
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(base.Students.Count>=5)
+            {
+                if (averageGrade >= 90)
+                    return 'A';
+                else if (averageGrade >= 80)
+                    return 'B';
+                else if (averageGrade >= 70)
+                    return 'C';
+                else if (averageGrade >= 60)
+                    return 'D';
+                else
+                    return 'F';
+            }
+            else
+            {
+                return 'F';
+            }
+        }
     }
 }
